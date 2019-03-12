42430641122c87ad7596e768901ec8438cb7b9a1
Lara Vazquez <lvazquezdorna@danielcastelao.org>
2019-03-12 20:59:46 +0100

    1.INIT

---
+++ src/chatlara/Client.java
@@ -0,0 +1,77 @@
+package chatlara;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+
+    public static void main(String[] args) {
+
+        final Socket clientSocket;
+        final BufferedReader in;
+        final PrintWriter out;
+        final Scanner sc = new Scanner(System.in);//leer desde teclado
+        int Puerto = 500;
+        String Host = "127.0.0.1";
+        Thread enviar;
+        Thread recibir;
+        String nombre="Cliente";
+        try {
+            /*
+             * información del servidor (puerto y dirección IP o nombre del host)
+             * 127.0.0.0.0.1 es la dirección local de la máquina
+             */
+            clientSocket = new Socket(Host, Puerto);
+
+            //flujo de salida 
+            out = new PrintWriter(clientSocket.getOutputStream());
+            //flujo de entrada
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            //crea un nuevo hilo que envia lo que escribe 
+            enviar = new Thread(new Runnable() {
+                String msg;
+
+                @Override
+                public void run() {
+                    while (true) {
+                        msg = sc.nextLine();
+                        out.println(nombre + ": " + msg);
+                        out.flush();
+                    }
+                }
+            });
+            enviar.start();
+
+            //crea un hilo que recibe lo que escribe desde el servidor
+            recibir = new Thread(new Runnable() {
+                String msg;
+
+                @Override
+                public void run() {
+                    try {
+                        msg = in.readLine();
+                        while (msg != null) {
+                            System.out.println(msg);
+                            msg = in.readLine();
+                        }
+                        System.out.println("Servidor desconectado");
+                        out.close();
+                        clientSocket.close();
+                    } catch (IOException e) {
+                        System.out.println("ERROR durante la lectura");
+                    }
+                }
+            });
+            recibir.start();
+
+        } catch (IOException e) {
+            System.out.println("ERROR! No se pudo conectar");
+        }
+    }
+}
---
+++ src/chatlara/Server.java
@@ -0,0 +1,100 @@
+package chatlara;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/*
+ * www.codeurjava.com
+ */
+
+public class Server {
+
+    public static void main(String[] test) {
+
+        final ServerSocket Chat;
+        final Socket clientSocket;
+        final BufferedReader in;
+        final PrintWriter out;
+        final Scanner sc = new Scanner(System.in);
+        int Puerto = 500;
+
+        Thread enviar;
+        Thread recibir;
+        String nombre = "Servidor";
+
+        try {
+            //inicia el Socket del servidor
+            Chat = new ServerSocket(Puerto);
+
+            //inicia la conexion con el cliente
+            clientSocket = Chat.accept();
+            //inicia los buffers de entrada y salida
+            out = new PrintWriter(clientSocket.getOutputStream());
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            enviar = new Thread(new Runnable() {
+                String msg;
+
+                @Override
+                public void run() {
+                    while (true) {
+                        msg = sc.nextLine();
+                        out.println(msg);
+                        out.flush();
+                        if (msg.equalsIgnoreCase("q")) {
+                            try {
+                                //cierra conexion con el cliente
+                                clientSocket.close();
+                                //cierra servidor
+                                Chat.close();
+                            } catch (IOException ex) {
+                                Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+                            }
+                        }
+                    }
+                }
+            });
+
+           //inicia el hilo
+            enviar.start();
+
+            recibir = new Thread(new Runnable() {
+                String msg;
+
+                @Override
+                public void run() {
+                    try {
+                        msg = in.readLine();
+                        //siempre y cuando el cliente esté conectado
+                        while (msg != null) {
+                            System.out.println(nombre + ": " + msg);
+                            msg = in.readLine();
+                        }
+                        //salir del bucle si el cliente se ha desconectado
+                        System.out.println("Cliente desconectado");
+                        //cierra el buffer
+                        out.close();
+                        //cierra conexion con el cliente
+                        clientSocket.close();
+                        //cierra servidor
+                        Chat.close();
+                    } catch (IOException e) {
+                        System.out.println("ERROR durante la lectura");
+                    }
+                }
+            });
+
+            //escucha lo que le envian por el servidor
+            recibir.start();
+        } catch (IOException e) {
+            System.out.println("ERROR! No se pudo conectar");
+        }
+    }
+}
